1b0398610604712e65a0274f205f4390ad0428f7
Santiago <santiago.videla@gmail.com>
2016-08-04 10:45:51 -0300

    .travis.yml

--- clingo/build.sh
+++ clingo/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-scons --build-dir=release
+$PREFIX/bin/scons CXX=COMPILER --build-dir=release
 mv build/release/python/gringo.so $PREFIX/lib/python2.7/site-packages
 mv build/release/clingo build/release/gringo build/release/reify $PREFIX/bin
