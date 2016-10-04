9e045b309ff6afe967bef6fe928f63a29036e253
Santiago <santiago.videla@gmail.com>
2016-10-04 15:52:26 -0300

    upgrade to clingo-5.0.0

--- clingo/build.sh
+++ clingo/build.sh
@@ -2,5 +2,5 @@
 
 $PREFIX/bin/scons --build-dir=release
 
-mv build/release/python/gringo.so $PREFIX/lib/python2.7/site-packages
+mv build/release/python/clingo.so $PREFIX/lib/python2.7/site-packages
 mv build/release/clingo build/release/gringo build/release/reify $PREFIX/bin
